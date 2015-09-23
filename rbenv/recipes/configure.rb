12e96974008e7b7587060a8748b7dbfc7b74a4b6
chil <luchihli@gmail.com>
2015-09-22 21:25:33 -0400

    added database.yml

--- rbenv/recipes/configure.rb
+++ rbenv/recipes/configure.rb
@@ -6,15 +6,20 @@ script "install_rbenv_ruby221" do
     id > 1.txt
     date >> 1.txt
     echo "Hello" >> 1.txt
-    yum install -y libffi-devel libxml2 libxml2-devel libxslt libxslt-devel  readline-devel
+    yum install -y libffi-devel libxml2 libxml2-devel libxslt libxslt-devel  readline-devel nodejs
 
+    echo "done Yum" >> 1.txt
     cd /home
     tar -zxf /opt/aws/opsworks/current/merged-cookbooks/deployhome.tar.gz 
     chown -R deploy /home/deploy
+    echo "done tar" >> 1.txt
     
     rm -f /usr/local/bin/ruby
     ln -s /home/deploy/.rbenv/shims/ruby  /usr/local/bin/
     gem install bundler
+    gem install rails
+    gem install execjs
+    echo "done links" >> 1.txt
 
   EOH
 end
