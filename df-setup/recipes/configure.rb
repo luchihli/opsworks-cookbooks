7121da705ef74d96eaa2f0f8f8de8fff047260b8
luchihli-df <chihli@designfeed.io>
2015-09-25 19:22:02 -0400

    yes

--- df-setup/recipes/configure.rb
+++ df-setup/recipes/configure.rb
@@ -6,8 +6,10 @@ script "df-setup" do
     echo "#-#-#-#-#-# luchihli Setup"
     id > setup.txt
     env > setup.txt
+   
+    chown -R deploy  /usr/local/lib/ruby/gems/2.2.0
 
-    gem install stripe --source https://github.com/stripe/stripe-ruby
+#    gem install stripe -v '1.27.1'  --source https://github.com/stripe/stripe-ruby --install-dir /home/deploy/.bundler/df_app
     
 
     #cd /home
